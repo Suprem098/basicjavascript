95826e7a255e791295edab91fe30f48115444372
supremgiri_10 <128827134+Suprem098@users.noreply.github.com>
2024-06-26 09:15:54 +0545

    Add files via upload

---
+++ index.js
@@ -0,0 +1,121 @@
+//  variables
+// var x=90;
+// console.log(x);
+// var x=500;
+// console.log(x);
+
+// let y=90;
+// console.log(y);
+//  y=500;
+// console.log(y);
+
+// const result=9+9
+// console.log(result);
+// const result=6-3;
+// comsole.log(result);
+
+// data types
+// let x=90;
+// console.log(x);
+// console.log(typeof x);
+
+// string
+// const result= "suprem giri";
+// console.log(result);
+// console.log(typeof result);
+
+// undefined
+// let a;
+// console.log(a);
+// console.log(typeof a);
+
+// null
+// const y=null;
+// console.log(y)
+// console.log(typeof y);
+
+// boolean
+// const istrue=true;
+// console.log(istrue);
+// console.log(typeof istrue)
+
+// float
+// const result=5.78;
+// console.log(result);
+// console.log(typeof result);
+
+// string
+//  const output="suprem giri"
+//  console.log(output);
+//  console.log(typeof output);
+
+// boolean
+
+// const ismarried =false;
+// console.log(ismarried);
+// console.log(typeof ismarried);
+
+//  undefined
+// let a;
+// console.log(a);
+// console.log(typeof a);
+
+// null
+
+// const x= null;
+// console.log(x);
+// console.log(typeof x);
+// \bigInt
+// const x= 754065108n;
+// console.log(x);
+// console.log(typeof x);
+
+// symbol
+//  const result = Symbol ("who are you");
+//     console.log(result);
+//     console.log(typeof result);
+
+// Object type
+
+// const obj ={
+//     fname:"suprem giri",
+//     age:19,
+//     Ismarried:false,
+//     occupation:"developer",
+//     salary:45000,
+// };
+// console.log(obj);
+// console.log(typeof obj);
+
+// const arr =[3,4,5,6,7];
+// console.log(arr);
+// console.log(typeof arr); 
+
+// const date = new Date();
+// console.log(date);
+// console.log(typeof date);
+
+
+
+// type conversion
+
+// const x = "123";
+// const val = Number(x);
+// console.log(val);
+// console.log(typeof val);
+
+
+// const result = "8.98";
+// const output = parseFloat(result);
+// console.log(output);
+// console.log(typeof output);
+
+// const result= 1+"1";
+// console.log(result);
+// console.log(typeof result); 
+
+const result= 8-"1";
+console.log(result);
+console.log(typeof result); 
+
+
